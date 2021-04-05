62a7639bed1e2dfeb4ae5b082ce73364a144bdbb
kishorevbhosale <kishorevbhosale@gmail.com>
2021-04-05 22:37:01 +0530

    array rotation examples

---
+++ src/array/StrRotationsOfEachOther.java
@@ -0,0 +1,14 @@
+package array;
+
+public class StrRotationsOfEachOther {
+    public static void main(String[] args) {
+        String str1 = "ABCD";
+        String str2 = "CDBA";
+        String tmp = str1.concat(str2);
+        if (tmp.contains(str2)){
+            System.out.println("strings are rotations of each other");
+        }else {
+            System.out.println("strings are not rotations of each other");
+        }
+    }
+}
