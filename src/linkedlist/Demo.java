e4d7faf4de17029f0988eb04b1f97c645db19994
kishorevbhosale <kishorevbhosale@gmail.com>
2021-05-15 16:46:23 +0530

    commented lines removed

--- src/linkedlist/Demo.java
+++ src/linkedlist/Demo.java
@@ -5,17 +5,7 @@ import java.util.Map;
 public class Demo {
     public static void main(String[] args) {
         Map<String, String> hashMap = null;
-
-        /*hashMap.put(null, "#FF0000");
-        hashMap.put("BLACK", null);
-        hashMap.put("BLUE", "#0000FF");
-        hashMap.put("GREEN", "#008000");
-        hashMap.put("WHITE", null);
-        //hashMap = hashMap.entrySet().removeIf(Objects::isNull);
-        hashMap = hashMap.entrySet().removeIf(key->key.getKey()==null);*/
         test(hashMap);
-
-
     }
 
     private static void test(Map<String, String> hashMap) {
