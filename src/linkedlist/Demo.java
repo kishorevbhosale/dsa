9380500ce5547e02967aef172123d535154c7925
kishorevbhosale <kishorevbhosale@gmail.com>
2021-05-15 16:45:25 +0530

    demo commit

--- src/linkedlist/Demo.java
+++ src/linkedlist/Demo.java
@@ -1,4 +1,26 @@
 package linkedlist;
 
+import java.util.Map;
+
 public class Demo {
+    public static void main(String[] args) {
+        Map<String, String> hashMap = null;
+
+        /*hashMap.put(null, "#FF0000");
+        hashMap.put("BLACK", null);
+        hashMap.put("BLUE", "#0000FF");
+        hashMap.put("GREEN", "#008000");
+        hashMap.put("WHITE", null);
+        //hashMap = hashMap.entrySet().removeIf(Objects::isNull);
+        hashMap = hashMap.entrySet().removeIf(key->key.getKey()==null);*/
+        test(hashMap);
+
+
+    }
+
+    private static void test(Map<String, String> hashMap) {
+        if (hashMap == null) {
+            System.out.println("null");
+        }
+    }
 }
