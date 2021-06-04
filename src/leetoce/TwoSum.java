e7047026c8a7453208ea238722efe18fa893a695
kishorevbhosale <kishorevbhosale@gmail.com>
2021-06-04 22:49:22 +0530

    Two Sum

---
+++ src/leetoce/TwoSum.java
@@ -0,0 +1,26 @@
+package leetoce;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int k = 0; k < nums.length; k++) {
+            for (int l = k + 1; l < nums.length; l++) {
+                if (nums[k] + nums[l] == target) {
+                    result[0] = k;
+                    result[1] = l;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
