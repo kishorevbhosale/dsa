7a2e9e462a2951896a5de20972885c3c0dba4160
kishorevbhosale <kishorevbhosale@gmail.com>
2021-06-17 08:49:11 +0530

    First DP problem

---
+++ src/leetcode/LongestSubStringUsingDP.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+public class LongestSubStringUsingDP {
+    public static void main(String[] args) {
+        String str1 = "qqppwqqppp";
+        String str2 = "babar";
+        String str = "zudfweormatjycujjirzjpyrmaxurectxrtqedmmgergwdvjmjtstdhcihacqnothgttgqfywcpgnuvwglvfiuxteopoyizgehkwuvvkqxbnufkcbodlhdmbqyghkojrgokpwdhtdrwmvdegwycecrgjvuexlguayzcammupgeskrvpthrmwqaqsdcgycdupykppiyhwzwcplivjnnvwhqkkxildtyjltklcokcrgqnnwzzeuqioyahqpuskkpbxhvzvqyhlegmoviogzwuiqahiouhnecjwysmtarjjdjqdrkljawzasriouuiqkcwwqsxifbndjmyprdozhwaoibpqrthpcjphgsfbeqrqqoqiqqdicvybzxhklehzzapbvcyleljawowluqgxxwlrymzojshlwkmzwpixgfjljkmwdtjeabgyrpbqyyykmoaqdambpkyyvukalbrzoyoufjqeftniddsfqnilxlplselqatdgjziphvrbokofvuerpsvqmzakbyzxtxvyanvjpfyvyiivqusfrsufjanmfibgrkwtiuoykiavpbqeyfsuteuxxjiyxvlvgmehycdvxdorpepmsinvmyzeqeiikajopqedyopirmhymozernxzaueljjrhcsofwyddkpnvcvzixdjknikyhzmstvbducjcoyoeoaqruuewclzqqqxzpgykrkygxnmlsrjudoaejxkipkgmcoqtxhelvsizgdwdyjwuumazxfstoaxeqqxoqezakdqjwpkrbldpcbbxexquqrznavcrprnydufsidakvrpuzgfisdxreldbqfizngtrilnbqboxwmwienlkmmiuifrvytukcqcpeqdwwucymgvyrektsnfijdcdoawbcwkkjkqwzffnuqituihjaklvthulmcjrhqcyzvekzqlxgddjoir";
+        System.out.println(longestPalindrome(str));
+    }
+
+    public static String longestPalindrome(String str) {
+        int maxLength = 1;
+        int n = str.length();
+        boolean[][] table = new boolean[n][n];
+
+        // for length = 1
+        for (int i = 0; i < n; ++i) {
+            table[i][i] = true;
+        }
+
+        // for length = 2
+        int start = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                table[i][i + 1] = true;
+                maxLength = 2;
+                start = i;
+            }
+        }
+
+        // for length >=3
+        for (int k = 3; k <= n; ++k) {
+            for (int i = 0; i < n - k + 1; ++i) {
+                int j = i + k - 1;
+                if (table[i + 1][j - 1] && str.charAt(i) == str.charAt(j)) {
+                    table[i][j] = true;
+                    if (k > maxLength) {
+                        maxLength = k;
+                        start = i;
+                    }
+                }
+            }
+
+        }
+        return str.substring(start, start + maxLength);
+    }
+}
+
