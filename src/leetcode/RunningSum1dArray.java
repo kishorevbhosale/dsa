6a38889ae393c27ef80e251a1b89ad0ea8789e57
kishorevbhosale <kishorevbhosale@gmail.com>
2021-06-19 07:49:34 +0530

    easy - leetcode - running sum of 1d array

---
+++ src/leetcode/RunningSum1dArray.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class RunningSum1dArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,7,9};
+
+        for (int num:runningSum(nums)){
+            System.out.print(num+" ");
+        }
+    }
+
+    private static int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            nums[i] = nums[i-1]+nums[i];
+        }
+        return nums;
+    }
+}
