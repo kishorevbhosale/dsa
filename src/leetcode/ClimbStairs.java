2c9679b977315f5c92a57e53c080dc4dcfd80ea4
kishorevbhosale <kishorevbhosale@gmail.com>
2021-06-26 16:05:09 +0530

    leetcode easy

---
+++ src/leetcode/ClimbStairs.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class ClimbStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+    }
+
+
+    private static int climbStairs(int n) {
+        if(n<4) return n;
+        int t0 = 2, t1 = 3, s = 0;
+        for (int i = 4; i <= n; i++) {
+            s = t0 + t1;
+            t0 = t1;
+            t1 = s;
+        }
+        return s;
+    }
+}
