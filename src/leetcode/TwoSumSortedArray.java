13114c1e646ccae469053d7f155bcaa5e8eb6580
kishorevbhosale <kishorevbhosale@gmail.com>
2021-06-30 19:46:08 +0530

    leetcode easy

---
+++ src/leetcode/TwoSumSortedArray.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class TwoSumSortedArray {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4, 4, 9, 56, 90}, 8)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target)
+                return new int[]{i + 1, j + 1};
+            else if (numbers[i] + numbers[j] < target)
+                i++;
+            else
+                j--;
+        }
+        return null;
+    }
+}
